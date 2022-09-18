5d118d6072b5ad4f045275c25540d952c4ea61c8
Alonso Paucar <iaquintanapa@gmail.com>
2022-09-18 08:33:18 -0500

    poniendo personajes de dbz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador/contador.component';
 
 import { HeroesModule } from './heroes/heroes.module';
 import { contadormodule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
   
 
@@ -16,7 +16,8 @@ import { contadormodule } from './contador/contador.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    contadormodule
+    contadormodule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
